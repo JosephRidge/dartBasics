d3f28badc00fde1d743283bc4bfe9251d2126db8
josephridge <josephridge18@gmail.com>
2023-06-29 08:16:42 +0300

    Guide

---
+++ prac.dart
@@ -0,0 +1,86 @@
+void main() {
+  /**
+   * # VARIABLES
+   * var
+   * Integer | String | Boolean | List
+   * dynamic -> not sure what it will be 
+   * const - immutable 
+   * final - single assignment at runtime
+   * -> Namin Aproach 
+   */
+
+  /**
+   * DATA TYPES
+   * Numbers | Strings | Booleans  | Lists(Arrays) | Maps(key-value pairs) | Dynamic
+   * 
+   */
+
+  //String concatenations
+
+  /**
+     * LISTS - Arrays
+     * create -> var list = [1,2,3]
+     * create empty list  -> list =[]
+     * accessing lists using index
+     * element reassignment => list[index] = newValue
+     * add one element to list using .add(element)
+     * add many elements to list .addAll([1,2,3])
+     * insert at particular position .insert(position, item) => overwrites it 
+     * u can have a mixed data type list eg[1,2,"Hello", "World"]
+     * remove(elementNam) or removeAt(index)
+     */
+
+/**
+ * MAPS
+ * create -> var map ={key:value,}
+ * Show =>  print(map), print(map[key])
+ * keys => print(map.keys)
+ * values => print(map.values)
+ * length => map.length
+ * add => map[newKey] = newValue
+ * add many => map.addAll({key:value, key:value}) 
+ * remove all> map.clear()
+ * remove one => map.clear(key)
+ * 
+ */
+
+  /**
+   * LOOPS 
+   * for (var x=0;  x<100; x++)
+   * while =>while(condition) {}
+   * for in => for (item in items) //do something
+   */
+
+  /**
+   * LOGIC
+   * if()
+   * if..else{}
+   * if..else if...else
+   */
+
+  /**
+   * DISCRETE 
+   * && , ||, ! , 
+   * truth tables 
+   */
+
+  /**
+   * FUNCTIONS (parameterized and non parameterized)
+   * functionName(){}
+   * functionName(var x)
+   */
+
+  /**
+   * String conversions 
+   * int.parse()
+   * .toString()
+   * double.parse()
+   */
+
+  /**CHALLENGE
+   * 
+   * Print numbers 1 to 100
+   * fizz buzz 
+   * https://www.hackerrank.com/challenges/fizzbuzz/problem
+   */
+}
